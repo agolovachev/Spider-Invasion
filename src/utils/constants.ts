1deae713bae904e4767d76eae4005bd58e6dcd2b
agolovachev <golovachevandrey134@gmail.com>
2026-04-14 14:59:43 +0500

    Costants tuned

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
 export const TILE_WIDTH = 64;
 export const TILE_HEIGHT = 32;
-export const GRID_SIZE = 20;
+export const GRID_SIZE = 40;
 
-export const SPIDER_SPEED = 80;
-export const CATCH_RADIUS = 40;
+export const SPIDER_SPEED = 10;
+export const CATCH_RADIUS = 4;
 export const CARGO_LIMIT = 5;
 export const RESPAWN_DELAY = 5000; // ms
-export const INSECTS_PER_SPAWN = 3;
+export const INSECTS_PER_SPAWN = 5;
 
 export const CATCH_TIMES = {
   fly: 2,
